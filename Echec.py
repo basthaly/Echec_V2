d929c040ac59c108c437335409e06bfc2665fffc
Basthaly <bastien48@live.fr>
2020-09-01 16:28:00 +0200

    01/09

--- Echec.py
+++ Echec.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/python
+# -*-coding:Latin-1 -* 
 
 class Jeu_Echec:
     def __init__(self):
@@ -33,32 +34,46 @@ class Jeu_Echec:
         self.echiquier[6][7]=(3) #Cavalier
         self.echiquier[7][7]=(2) #Tour
         for i in range (8):
-            self.echiquier[i][1]=(1) #pion
+            self.echiquier[i][6]=(1) #pion
 
     def __str__(self):
-        a="|"
+        a="Tour : "+str(self.tour)+"\n"
+        for j in range(1,9):
+            a=a+"   "+str(j)+"  "
+        a=a+"\n"
         for i in range(8):
             for j in range(8):
-                a=a+" "(self.echiquier[i][j])" |"
-            a=a+"/n"
-        a=a+"/n"
+                if self.echiquier[i][j]<0:
+                    a=a+"| "+str(self.echiquier[i][j])+" |"
+                else:
+                    a=a+"|  "+str(self.echiquier[i][j])+" |"
+            a=a+" "+chr(72-i)+"\n"
+        a=a+"\n"
         return a
 
-    def Tr_pos(pos): #Récupère H4 et renvoie (8,4)
-        for i in range (65,72,1):
-            if i==ord(pos[0]):
-                x=i
+    def Tr_pos(self,pos): #Récupère H4 et renvoie (8,4)
+        for i in range (65,73):
+            if chr(i)==pos[0]:
+                x=i-65
                 break
-        return (x,pos[1]-1)
+        return (x,int(pos[1])-1)
 
     def action(self,p1,p2):
         self.tour+=1
         self.echiquier[p2[0]][p2[1]]=self.echiquier[p1[0]][p1[1]]
-        self.echiquier[p2[0]][p2[1]]=(0)
+        self.echiquier[p1[0]][p1[1]]=(0)
+    
+    def deplac(self,p1,p2):
+        pos1=self.Tr_pos(p1)
+        pos2=self.Tr_pos(p2)
+        self.action(pos1,pos2)
+
 
 
 ##### TEST #####
 jeu=Jeu_Echec()
+print(jeu)
+jeu.deplac("A1","H8")
 print(jeu)
 
 
