b74efc38023c1fd408f24047efe09734cbc98655
basthaly <bastien48@live.fr>
2023-03-31 20:12:07 +0200

    Version 1.2

--- Echec.py
+++ Echec.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
 # -*-coding:Latin-1 -* 
 
-"""
-Dans ce programme, les pièces d'échecs sont représentées par des entiers sur l'échiquier.
-Les pièces blanches ont des valeurs positives, tandis que les pièces noires ont des valeurs négatives.
-Les valeurs absolues des entiers correspondent aux pièces comme suit :
 
-1 (ou -1) : Pion
-2 (ou -2) : Tour
-3 (ou -3) : Cavalier
-4 (ou -4) : Fou
-5 (ou -5) : Dame
-6 (ou -6) : Roi
-
-Les cases vides de l'échiquier sont représentées par le chiffre 0.
-Ainsi, chaque case de l'échiquier contient un entier.
-Cette entier indique la pièce présente sur cette case et la couleur de cette pièce (positive pour les Blancs, négative pour les Noirs).
-"""
+## Import ici
+## Fin des import
 
 class Jeu_Echec:
+    """
+    Dans ce programme, les pièces d'échecs sont représentées par des entiers sur l'échiquier.
+    Les pièces blanches ont des valeurs positives, tandis que les pièces noires ont des valeurs négatives.
+    Les valeurs absolues des entiers correspondent aux pièces comme suit :
+
+    1 (ou -1) : Pion
+    2 (ou -2) : Tour
+    3 (ou -3) : Cavalier
+    4 (ou -4) : Fou
+    5 (ou -5) : Dame
+    6 (ou -6) : Roi
+
+    Les cases vides de l'échiquier sont représentées par le chiffre 0.
+    Ainsi, chaque case de l'échiquier contient un entier.
+    Cette entier indique la pièce présente sur cette case et la couleur de cette pièce (positive pour les Blancs, négative pour les Noirs).
+    """
     def __init__(self):
         self.echiquier=[]
         self.tour=0
@@ -54,6 +57,25 @@ class Jeu_Echec:
             a = a + " " + chr(72 - i) + "\n"
         a = a + "\n"
         return a
+    
+    def piece_nom(self, piece):
+        nom_piece_dict = {
+            1: "pion",
+            2: "tour",
+            3: "cavalier",
+            4: "fou",
+            5: "roi",
+            6: "reine",
+        }
+        return nom_piece_dict.get(piece, "")
+    
+    def piece_couleur(self, piece):
+        if piece == 0:
+            return None
+        elif piece > 0:
+            return 1
+        else:
+            return -1
 
     def Tr_pos(self,pos): #Récupère H4 et renvoie (8,4)
         for i in range (65,73):
@@ -65,11 +87,6 @@ class Jeu_Echec:
 
     def est_dans_echiquier(self, x, y):
         return 0 <= x < 8 and 0 <= y < 8
-
-    # def action(self,p1,p2):
-    #     self.tour+=1
-    #     self.echiquier[p2[0]][p2[1]]=self.echiquier[p1[0]][p1[1]]
-    #     self.echiquier[p1[0]][p1[1]]=(0)
     
     def action(self, p1, p2, joueur):
         if self.echiquier[p1[0]][p1[1]] * joueur > 0:  # Vérifie si la pièce appartient au joueur actuel
@@ -78,22 +95,9 @@ class Jeu_Echec:
             self.echiquier[p1[0]][p1[1]] = 0
             self.joueur = -joueur  # Change le joueur actuel
         else:
-            print("C'est au tour du joueur", joueur, "de jouer.")
+            print ("C'est au tour du joueur", joueur, "de jouer.")
 
-    def deplac(self,p1,p2):
+    def deplace(self,p1,p2):
         pos1=self.Tr_pos(p1)
         pos2=self.Tr_pos(p2)
-        self.action(pos1,pos2)
-
-
-
-# Exemple d'utilisation
-if __name__ == "__main__":
-    jeu = Jeu_Echec()
-    print(jeu)
-
-    # Effectuer un déplacement
-    jeu.deplac("A1", "A2")
-    print(jeu)
-
-
+        self.action(pos1,pos2,self.joueur)
---
+++ EchecGUI.py
@@ -0,0 +1,120 @@
+from Echec import Jeu_Echec
+
+import tkinter as tk
+from PIL import Image, ImageTk
+
+class FenetrePersonnalisee(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Jeu d'échecs")
+        self.geometry("854x480")
+        self.minsize(200, 200)
+        self.case_taille=50
+
+        self.canvas = tk.Canvas(self, width=854, height=480)
+        self.canvas.pack()
+
+        self.jeu = Jeu_Echec()
+        
+        self.menu()
+
+        self.canvas.bind("<Configure>", self.redessiner_echiquier)
+        self.bind("<Configure>", self.on_resize)
+        self.update_idletasks()
+
+        self.inverser_echiquier = False
+        self.fullscreen = False
+        self.save_taille = (0,0)
+        self.piece_selectionnee = None
+
+        self.mise_a_jour_piece_images()
+
+        self.redessiner_echiquier()
+
+    def on_resize(self, event=None):
+        self.case_taille = self.calculer_case_taille()
+        self.canvas.config(width=event.width, height=event.height)  # Mettre à jour la taille du canvas
+        self.redessiner_echiquier()
+
+    def menu(self):
+        menubar = tk.Menu(self)
+
+        options_menu = tk.Menu(menubar, tearoff=0)
+        options_menu.add_command(label="Inverser échiquier", command=self.inverser)
+        menubar.add_cascade(label="Options", menu=options_menu)
+
+        self.resolution_menu = tk.Menu(menubar, tearoff=0)
+        self.resolution_menu.add_command(label="480p", command=lambda: self.changer_taille(854, 480))
+        self.resolution_menu.add_command(label="720p", command=lambda: self.changer_taille(1280, 720))
+        self.resolution_menu.add_command(label="1080p", command=lambda: self.changer_taille(1920, 1080))
+        self.resolution_menu.add_separator()
+        self.resolution_menu.add_command(label="Plein écran", command=self.mode_plein_ecran)
+        menubar.add_cascade(label="Résolution", menu=self.resolution_menu)
+
+        self.config(menu=menubar)
+
+    def calculer_case_taille(self):
+        largeur = self.winfo_width()
+        hauteur = self.winfo_height()
+        return min(largeur, hauteur) // 8
+
+    def dessiner_echiquier(self):
+        for i in range(8):
+            for j in range(8):
+                if self.inverser_echiquier:
+                    x, y = (7 - i) * self.case_taille, (7 - j) * self.case_taille
+                else:
+                    x, y = i * self.case_taille, j * self.case_taille
+                couleur = "white" if (i + j) % 2 == 0 else "gray"
+                self.canvas.create_rectangle(x, y, x + self.case_taille, y + self.case_taille, fill=couleur)
+                # self.canvas.create_image(x, y, image=image, anchor=tk.NW, tags=("piece",))
+
+                piece = self.jeu.echiquier[j][i]
+                if piece != 0:
+                    couleur = "blanc" if piece > 0 else "noir"
+                    nom_piece = self.jeu.piece_nom(abs(piece))
+                    image = self.piece_images[f"{couleur}_{nom_piece}"]
+                    self.canvas.create_image(x, y, image=image, anchor=tk.NW)
+
+    def redessiner_echiquier(self, event=None):
+        self.canvas.delete("all")
+        self.mise_a_jour_piece_images()
+        self.dessiner_echiquier()    
+
+    def changer_taille(self, largeur, hauteur):
+        self.geometry(f"{largeur}x{hauteur}")
+        self.canvas.config(width=largeur, height=hauteur)
+        self.canvas.update()
+
+    def mode_plein_ecran(self):
+        self.fullscreen = not self.fullscreen
+        self.attributes('-fullscreen', self.fullscreen)
+        self.resolution_menu.entryconfig("480p", state=tk.DISABLED if self.fullscreen else tk.NORMAL)
+        self.resolution_menu.entryconfig("720p", state=tk.DISABLED if self.fullscreen else tk.NORMAL)
+        self.resolution_menu.entryconfig("1080p", state=tk.DISABLED if self.fullscreen else tk.NORMAL)
+
+    def inverser(self):
+        self.inverser_echiquier = not self.inverser_echiquier
+        self.redessiner_echiquier()
+
+    def charger_piece_images(self):
+        piece_images = {}
+        pieces = ["pion", "tour", "cavalier", "fou", "roi", "reine"]
+        couleurs = ["blanc", "noir"]
+
+        for piece in pieces:
+            for couleur in couleurs:
+                chemin = f"//nas-bastha/home/Drive/Travail/Echec_v2/images/pieces/{couleur}_{piece}.png"
+                image = Image.open(chemin)
+                image = image.resize((self.case_taille, self.case_taille), Image.LANCZOS)
+                piece_images[f"{couleur}_{piece}"] = ImageTk.PhotoImage(image)
+
+        return piece_images
+    
+    def mise_a_jour_piece_images(self):
+        self.piece_images = self.charger_piece_images()
+
+if __name__ == "__main__":
+    app = FenetrePersonnalisee()
+    app.mainloop()
